0431ad3a82b2a7f879f6a507335017fb9114a69f
Dag <dag.wastberg@ramboll.se>
2021-05-13 17:43:38 +0200

    Initial commit MeshMesh intersection

---
+++ MeshMeshIntersect.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rhino.Geometry;
+using Embree;
+
+namespace GHEmbree
+{
+    class MeshMeshIntersect
+    {
+        public static bool TestIntersect(Mesh meshA, Mesh meshB, bool fast = true)
+        {
+            bool meshInterects = false;
+
+            bool bboxIntersection = TestBBIntersect(meshA, meshB);
+            if (!bboxIntersection)
+            {
+                return false; // if bounding boxes don't intersect then meshes don't interesect
+            }
+            Mesh largeMesh;
+            Mesh smallMesh;
+            if (meshA.Faces.Count > meshB.Faces.Count)
+            {
+                largeMesh = meshA;
+                smallMesh = meshB;
+            } else
+            {
+                largeMesh = meshB;
+                smallMesh = meshA;
+            }
+
+            var scene = EmbreeTools.BuildScene(largeMesh);
+
+            var meshVertices = smallMesh.Vertices.ToPoint3fArray();
+            foreach (var face in smallMesh.Faces)
+            {
+                int numVertices = face.IsTriangle ? 3 : 4;
+                for (int v = 0; v < numVertices; v++)
+                {   
+                    var ray = RayFromVertices(meshVertices[face[v]], meshVertices[face[(v+1)%numVertices]]);
+                    var packet = scene.Intersects(ray.Item1, 0, ray.Item2);
+                    bool hit = (packet.geomID != RTC.InvalidGeometryID);
+                    if (hit)
+                    {
+                        meshInterects = true;
+                        break;
+                    }
+                }
+                if (meshInterects)
+                {
+                    break;
+                }
+            }
+
+            return meshInterects;
+
+        }
+
+        private static Tuple<Ray,float> RayFromVertices(Point3f a, Point3f b)
+        {
+
+            EPoint pt = new EPoint(a.X, a.Y, a.Z);
+            EVector vec = new EVector(b.X - a.X, b.Y - a.Y, b.Z - a.Z);
+            Ray ray = new Ray(pt, vec);
+            float mag = EVector.Length(vec);
+            return new Tuple<Ray, float>(ray, mag);
+
+        }
+
+        static bool TestBBIntersect(Mesh meshA, Mesh meshB, bool fast = true)
+        {
+            var BboxA = meshA.GetBoundingBox(!fast);
+            var BboxB = meshB.GetBoundingBox(!fast);
+            return BBoxIntersect(BboxA, BboxB);
+
+        }
+
+        static bool BBoxIntersect(BoundingBox bboxA, BoundingBox bboxB)
+        {
+            if (bboxA.Max.X < bboxB.Min.X) return false; 
+            if (bboxA.Min.X > bboxB.Max.X) return false; 
+            if (bboxA.Max.Y < bboxB.Min.Y) return false; 
+            if (bboxA.Min.Y > bboxB.Max.Y) return false;
+            if (bboxA.Max.Z < bboxB.Min.Z) return false;
+            if (bboxA.Min.Z > bboxB.Max.Z) return false;
+            return true; // boxes overlap
+        }
+    }
+}
---
+++ MeshMeshIntersectsComponent.cs
@@ -0,0 +1,79 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+
+namespace GHEmbree
+{
+    public class MeshMeshIntersectsComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public MeshMeshIntersectsComponent()
+          : base("MeshMeshIntesects", "M|M",
+              "Test for intersection between two meshes",
+              "Mesh", "GHEmbree")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddMeshParameter("MeshA", "A", "Mesh A", GH_ParamAccess.item);
+            pManager.AddMeshParameter("MeshB", "B", "Mesh B", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Fast", "F", "Use faster method, can give false negative in some corner cases", GH_ParamAccess.item,true);
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Intersect", "I", "Do meshes intersect", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var meshA = new Rhino.Geometry.Mesh();
+            var meshB = new Rhino.Geometry.Mesh();
+            bool fastApprox = false;
+
+            if (!DA.GetData(0, ref meshA)) { return; }
+            if (!DA.GetData(1, ref meshB)) { return; }
+            if (!DA.GetData(2, ref fastApprox)) { return; }
+
+            bool intersects = MeshMeshIntersect.TestIntersect(meshA, meshB);
+
+            DA.SetData(0, intersects);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("12bff981-9d80-41e8-8964-789f1e959448"); }
+        }
+    }
+}
